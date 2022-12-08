e37313484dfee4c3c12abb64e09533d4495a45ef
Santosh Thomas <santhom@microsoft.com>
2022-12-08 11:23:32 -0800

    Changes as of 'Thu Dec  8 11:23:32 PST 2022'

--- scripts/make_test_data.py
+++ scripts/make_test_data.py
@@ -3,10 +3,24 @@ import json
 df = pd.read_csv("./data/ames_housing.csv")
 feature_columns = ["Lot Area", "Gr Liv Area", "Garage Area", "Bldg Type"]
 selected = df.loc[30:50, feature_columns]
+
 existing_values = selected.to_json(orient="split")
-new_dict = {'input_data': existing_values}
 filename = f'./data/test_data.json'
 
-# Write out test data
-with open(filename, 'w') as f:
-    json.dump(new_dict, f)
+# Two styles of getting JSON output
+# The second option takes out strings - which can sometimes cause an error at the endpoint
+option_1 = True # default
+#option_1 = False
+option_2 = False
+
+if option_1:
+    new_dict = {'input_data': existing_values}
+    # Write out test data
+    with open(filename, 'w') as f:
+        json.dump(new_dict, f)
+
+if option_2:
+    parsed = json.loads(existing_values)
+    # Write out test data
+    with open(filename, 'w') as f:
+        f.write(json.dumps({'input_data':parsed}))
